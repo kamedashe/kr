bfd68f59d52d4bcf110bce6b98bc5dbcb3d537d6
kamedashe <163843891+kamedashe@users.noreply.github.com>
2025-06-10 17:45:33 +0300

    Add dict-based ComponentDAO and service

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-
 from db.database import get_connection
 from ui.main_window import MainWindow
+from dao.supplier_dao import SupplierDAO
+from services.supplier_service import SupplierService
+from controllers.supplier_controller import SupplierController
+
 
 def main():
     app = MainWindow()
     conn = get_connection()
-    # TODO: instantiate DAOs, Services, Controllers and inject into tabs
+
+    supplier_dao = SupplierDAO(conn)
+    supplier_service = SupplierService(supplier_dao)
+    SupplierController(view=app.suppliers_tab, service=supplier_service)
+
     app.mainloop()
+
 
 if __name__ == "__main__":
     main()
+
