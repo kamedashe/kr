1025ab79d9f305339921924fcd9facef7ab1e72c
kamedashe <163843891+kamedashe@users.noreply.github.com>
2025-06-10 17:25:01 +0300

    Delete dao directory

--- dao/contract_dao.py
+++
@@ -1,4 +0,0 @@
-class ContractDAO:
-    """DAO для контрактів (заглушка)."""
-    def find_by_id(self, contract_id):
-        return None
--- dao/history_dao.py
+++
@@ -1,4 +0,0 @@
-class HistoryDAO:
-    """DAO для історії (заглушка)."""
-    def fetch_records(self, filters=None):
-        return []
