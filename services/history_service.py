073a3635ff2113983f255ce730a6d1958b1a2e91
kamedashe <163843891+kamedashe@users.noreply.github.com>
2025-06-10 17:24:38 +0300

    Delete services directory

--- services/history_service.py
+++
@@ -1,9 +0,0 @@
-from dao.supply_dao import SupplyDAO
-
-class HistoryService:
-    def __init__(self, history_dao):
-        self.history_dao = history_dao
-
-    def get_history(self):
-        return self.history_dao.fetch_records()
-
