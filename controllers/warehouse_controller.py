17ce27aa4abb9abb14973e560bc1c25e82e9094f
kamedashe <163843891+kamedashe@users.noreply.github.com>
2025-06-11 23:08:27 +0300

    Implement show_stock

--- controllers/warehouse_controller.py
+++ controllers/warehouse_controller.py
@@ -10,7 +10,8 @@ class WarehouseController:
 
     def show_stock(self):
         """Load current stock data into the view."""
-        self.view.refresh(self.service.list_all())
+        rows = self.facade.warehouse_dao.get_all_stock()
+        self.view.populate_stock(rows)
 
     def register_expense(self):
         """Register a component usage expense."""
