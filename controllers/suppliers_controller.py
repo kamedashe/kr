3623f063faaa90ce2b4e8b3b631b537217d933d7
kamedashe <163843891+kamedashe@users.noreply.github.com>
2025-06-11 22:00:07 +0300

    Implement list_all_suppliers method

--- controllers/suppliers_controller.py
+++ controllers/suppliers_controller.py
@@ -4,4 +4,6 @@ from controllers.supplier_controller import SupplierController
 class SuppliersController(SupplierController):
     """Alias controller matching naming convention."""
 
-    pass
+    def list_all_suppliers(self) -> list[tuple[int, str, str]]:
+        """Return all suppliers ordered by ``id`` ascending."""
+        return self.facade.supplier_dao.get_all()
